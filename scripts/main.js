7f231d5b31b7702ba3e049b46d375fff72efcfac
Kirill Vercetti <office@kyzima-spb.com>
2022-12-11 16:56:24 +0300

    Test

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,27 @@
 async function getLatestVersion(api, owner, repo) {
   const resp = await api.repos.listReleases({owner, repo})
-  const versions = resp.data.map(r => r.name)
-  versions.sort()
-  return versions[versions.length - 1]
+  const data = resp.data
+  
+  data.sort((a, b) => {
+    if (a.tag_name < b.tag_name) {
+      return -1
+    }
+    if (a.tag_name > b.tag_name) {
+      return 1
+    }
+    return 0
+  })
+  
+  return data[data.length - 1]
+  
+  // const versions = resp.data.map(r => r.name)
+  // versions.sort()
+  // return versions[versions.length - 1]
 }
 
 
 module.exports = async ({ github, core }) => {
   const latest = await getLatestVersion(github.rest, 'just-containers', 's6-overlay')
-  core.setOutput('value', latest)
+  console.log(latest)
+  core.setOutput('value', latest.tag_name)
 }
